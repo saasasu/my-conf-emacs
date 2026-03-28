0af9918e1ebed595b94708ecb061eece6bd73c29
saasasu <55926848+saasasu@users.noreply.github.com>
2026-03-28 17:06:27 +0100

    refactor into custom config file

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -1,19 +1,6 @@
-;;; -*- lexical-binding: t -*-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi))
- '(package-selected-packages '(expand-region ligature magit slime)))
+;; This file is extends the base .emacs file
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hasklug Nerd Font" :foundry "outline" :slant normal :weight regular :height 120 :width normal)))))
-
+;; Disable bell sound
 (setq ring-bell-function 'ignore)
 
 ;; Enable relative line numbers globally
